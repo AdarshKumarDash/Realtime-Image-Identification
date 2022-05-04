4fc375d8d2c9bb8874aeae3c1e423bca97c127ed
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-05-04 19:49:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,31 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier = ml5.imageClassifier("MobileNet", modelLoaded);
 }
 
+function modelLoaded() {
+  console.log("Model is loaded");
+}
 
+function draw() {
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, gotResult);
+}
 
+previous_result = "";
+
+function gotResult(error, result) {
+  if (error) {
+    console.log(error);
+  } else if ((result[0].confidence > 0.5) && (previous_result != result[0].label)) {
+    console.log(result);
+    previous_result = result[0].label;
+    synth = window.speechSynthesis;
+    speak_data = "object detected is: " + result[0].label;
+    utterance = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterance);
+    document.getElementById("result").innerHTML = result[0].label;
+    document.getElementById("confidence").innerHTML = Math.floor(result[0].confidence*100) + "%";
+  }
+}
